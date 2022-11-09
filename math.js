bf142644ff50bf4737d30c06048322f9f28cd0da
fouziawb <117811076+fouziawb@users.noreply.github.com>
2022-11-09 13:10:24 +0300

    Update math.js

--- math.js
+++ math.js
@@ -5,9 +5,3 @@ module.exports = {
   divide: (a, b) => a / b,
 };
 //adding feature 2
-let feature2 ="adding feature 2"
-// adding feature 3
-let feature3 = "adding feature 3
-//adding feature 1
-let feature1 = " adding feature 1"
-
